64ae85a24e1a30a9faac4ced97c1e99a76858aa1
mrjefflarry <mrjefflarry@gmail.com>
2022-06-13 00:47:34 +0200

    list project and sub project in a list

--- internal/cmd/project/list.go
+++ internal/cmd/project/list.go
@@ -9,6 +9,7 @@ import (
 	"github.com/MrJeffLarry/redmine-cli/internal/api"
 	"github.com/MrJeffLarry/redmine-cli/internal/config"
 	"github.com/MrJeffLarry/redmine-cli/internal/print"
+	"github.com/jedib0t/go-pretty/text"
 	"github.com/spf13/cobra"
 )
 
@@ -39,6 +40,9 @@ func displayListGET(r *config.Red_t, cmd *cobra.Command, path string) {
 	var status int
 	var idLen int
 	var nameLen int
+	var oldParent string
+	var parent string
+	var parentLevel int
 
 	projects := projects{}
 
@@ -81,8 +85,28 @@ func displayListGET(r *config.Red_t, cmd *cobra.Command, path string) {
 
 		idPad := strings.Repeat(" ", iLeft)
 		name := project.Name + strings.Repeat(" ", nLeft)
+		pName := project.Parent.Name
 
-		fmt.Printf("%s%s  %s\n", print.PrintID(project.ID), idPad, name)
+		if len(pName) > 0 && pName == parent {
+			// same level do nothing
+		} else if len(pName) > 0 && pName != parent {
+			if oldParent == pName {
+				parentLevel--
+			} else {
+				parentLevel++
+			}
+			oldParent = parent
+			parent = pName
+		} else {
+			parent = pName
+			parentLevel = 0
+		}
+
+		if parentLevel > 0 {
+			name = text.FgBlue.Sprint(project.Name + strings.Repeat(" ", nLeft))
+		}
+
+		fmt.Printf("%s%s %s %s\n", print.PrintID(project.ID), idPad, strings.Repeat(" ‣", parentLevel), name)
 	}
 	fmt.Printf("--- projects %d to %d (Total %d) ----\n",
 		projects.Offset,
@@ -97,7 +121,7 @@ func cmdProjectList(r *config.Red_t) *cobra.Command {
 		Short: "List projects",
 		Long:  "List all projects",
 		Run: func(cmd *cobra.Command, args []string) {
-			displayListGET(r, cmd, "/projects.json?")
+			displayListGET(r, cmd, "/projects.json?limit=1000")
 		},
 	}
 
@@ -107,7 +131,7 @@ func cmdProjectList(r *config.Red_t) *cobra.Command {
 		Short: "List all projects",
 		Long:  "List all projects",
 		Run: func(cmd *cobra.Command, args []string) {
-			displayListGET(r, cmd, "/projects.json?")
+			displayListGET(r, cmd, "/projects.json")
 		},
 	})
 
