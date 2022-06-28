13afd348d3acaa9cf96f9c410347dd9f7cacc791
mrjefflarry <mrjefflarry@gmail.com>
2022-06-28 16:38:36 +0200

    cleanup and bug fix

--- internal/api/client.go
+++ internal/api/client.go
@@ -10,6 +10,12 @@ import (
 	"github.com/MrJeffLarry/redmine-cli/internal/config"
 )
 
+const (
+	ERR_CONN_CREATE  = "Could not create connection with server, correct server details?"
+	ERR_CONN_SILENCE = "No response from server, correct server details?"
+	ERR_CONN_RES     = "Could not read response from server.."
+)
+
 func ClientGET(r *config.Red_t, path string) ([]byte, int, error) {
 	var err error
 	var res []byte
@@ -22,24 +28,24 @@ func ClientGET(r *config.Red_t, path string) ([]byte, int, error) {
 
 	req, err = http.NewRequest(http.MethodGet, r.RedmineURL+path, nil)
 	if err != nil {
-		fmt.Println(err.Error())
-		return res, statusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_CREATE)
 	}
+
 	req.Header.Add("X-Redmine-API-Key", r.RedmineApiKey)
 	resp, err = client.Do(req)
 	if err != nil {
-		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_SILENCE + "[" + r.RedmineURL + "]")
 	}
 	defer resp.Body.Close()
 
+	statusCode = resp.StatusCode
+
 	res, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Kunde inte typa redmines svar")
+		return res, statusCode, errors.New(ERR_CONN_RES)
 	}
 
-	return res, resp.StatusCode, nil
+	return res, statusCode, nil
 }
 
 func ClientPUT(r *config.Red_t, path string, body []byte) ([]byte, int, error) {
@@ -55,24 +61,28 @@ func ClientPUT(r *config.Red_t, path string, body []byte) ([]byte, int, error) {
 	req, err = http.NewRequest(http.MethodPut, r.RedmineURL+path, bytes.NewReader(body))
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, statusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_CREATE)
 	}
+
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("X-Redmine-API-Key", r.RedmineApiKey)
+
 	resp, err = client.Do(req)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_SILENCE + "[" + r.RedmineURL + "]")
 	}
 	defer resp.Body.Close()
+
+	statusCode = resp.StatusCode
 
 	res, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Kunde inte typa redmines svar")
+		return res, statusCode, errors.New(ERR_CONN_RES)
 	}
 
-	return res, resp.StatusCode, nil
+	return res, statusCode, nil
 }
 
 func ClientPOST(r *config.Red_t, path string) ([]byte, error) {
@@ -92,24 +102,26 @@ func ClientAuthBasicGET(r *config.Red_t, path, server, username, password string
 	req, err = http.NewRequest(http.MethodGet, server+path, nil)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, statusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_CREATE)
 	}
 	req.SetBasicAuth(username, password)
 
 	resp, err = client.Do(req)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_SILENCE + "[" + r.RedmineURL + "]")
 	}
 	defer resp.Body.Close()
+
+	statusCode = resp.StatusCode
 
 	res, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Could not read response from server, please try again")
+		return res, statusCode, errors.New(ERR_CONN_RES)
 	}
 
-	return res, resp.StatusCode, nil
+	return res, statusCode, nil
 }
 
 func ClientAuthApiKeyGET(r *config.Red_t, path, server, apikey string) ([]byte, int, error) {
@@ -125,22 +137,25 @@ func ClientAuthApiKeyGET(r *config.Red_t, path, server, apikey string) ([]byte, 
 	req, err = http.NewRequest(http.MethodGet, server+path, nil)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, statusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_CREATE)
 	}
+
 	req.Header.Add("X-Redmine-API-Key", apikey)
 
 	resp, err = client.Do(req)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Server could not handle our request")
+		return res, statusCode, errors.New(ERR_CONN_SILENCE + "[" + r.RedmineURL + "]")
 	}
 	defer resp.Body.Close()
+
+	statusCode = resp.StatusCode
 
 	res, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err.Error())
-		return res, resp.StatusCode, errors.New("Could not read response from server, please try again")
+		return res, statusCode, errors.New(ERR_CONN_RES)
 	}
 
-	return res, resp.StatusCode, nil
+	return res, statusCode, nil
 }
