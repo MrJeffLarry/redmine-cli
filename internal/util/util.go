08b55b54d04e23abd9c306517764cad0db7cb5cd
mrjefflarry <mrjefflarry@gmail.com>
2022-07-05 11:48:25 +0200

    Better IdName is on util as its used all over

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,5 +1,10 @@
 package util
 
+type IdName struct {
+	ID   int64  `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
 func Contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
