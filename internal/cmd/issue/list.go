6684fe8ab3fb22f04228d50a228d76f014ad3581
mrjefflarry <mrjefflarry@gmail.com>
2022-07-08 16:21:45 +0200

    display priority and will now sort by default on priority #9

--- internal/cmd/issue/list.go
+++ internal/cmd/issue/list.go
@@ -3,7 +3,6 @@ package issue
 import (
 	"encoding/json"
 	"fmt"
-	"math"
 	"net/url"
 	"strconv"
 	"strings"
@@ -17,6 +16,7 @@ import (
 
 const (
 	FLAG_ORDER_DESC = "desc"
+	FLAG_ORDER_ASC  = "asc"
 
 	FLAG_SORT   = "sort"
 	FLAG_SORT_P = "s"
@@ -32,6 +32,12 @@ const (
 
 	FLAG_PAGE   = "page"
 	FLAG_PAGE_P = "p"
+
+	HEAD_ID       = "ID"
+	HEAD_STATUS   = "STATUS"
+	HEAD_PRIORITY = "PRIORITY"
+	HEAD_PROJECT  = "PROJECT"
+	HEAD_SUBJECT  = "SUBJECT"
 )
 
 func countDigi(i int64) (count int) {
@@ -43,13 +49,13 @@ func countDigi(i int64) (count int) {
 }
 
 func parseFlags(r *config.Red_t, cmd *cobra.Command, path string) string {
-	FLAG_SORT_FIELDS := []string{"id", "status", "project", "subject"}
+	FLAG_SORT_FIELDS := []string{"id", "status", "priority", "project", "subject"}
 
 	limit, _ := cmd.Flags().GetInt(FLAG_LIMIT)
 	offset, _ := cmd.Flags().GetInt(FLAG_OFFSET)
 	page, _ := cmd.Flags().GetInt(FLAG_PAGE)
 	sort, _ := cmd.Flags().GetString(FLAG_SORT)
-	order, _ := cmd.Flags().GetBool(FLAG_ORDER_DESC)
+	order, _ := cmd.Flags().GetBool(FLAG_ORDER_ASC)
 	search, _ := cmd.Flags().GetString(FLAG_SEARCH)
 
 	if r.RedmineProjectID > 0 && !r.All {
@@ -69,11 +75,18 @@ func parseFlags(r *config.Red_t, cmd *cobra.Command, path string) string {
 
 	if util.Contains(FLAG_SORT_FIELDS, sort) {
 		path += "sort=" + sort
-		if order {
+		if !order {
+			path += ":desc"
+		}
+		path += "&"
+	} else {
+		path += "sort=priority"
+		if !order {
 			path += ":desc"
 		}
 		path += "&"
 	}
+
 	return path
 }
 
@@ -81,10 +94,11 @@ func displayListGET(r *config.Red_t, cmd *cobra.Command, path string) {
 	var err error
 	var body []byte
 	var status int
-	var statusLen int
-	var idLen int
-	var subjectLen int
-	var projectLen int
+	idLen := len(HEAD_ID)
+	statusLen := len(HEAD_STATUS)
+	priorityLen := len(HEAD_PRIORITY)
+	projectLen := len(HEAD_PROJECT)
+	subjectLen := len(HEAD_SUBJECT)
 
 	issues := issues{}
 
@@ -106,48 +120,57 @@ func displayListGET(r *config.Red_t, cmd *cobra.Command, path string) {
 	}
 
 	for _, issue := range issues.Issues {
-		subLen := len(issue.Subject)
-		proLen := len(issue.Project.Name)
-		sLen := len(issue.Status.Name)
 		iLen := countDigi(issue.ID)
+		sLen := len(issue.Status.Name)
+		pLen := len(issue.Priority.Name)
+		proLen := len(issue.Project.Name)
+		subLen := len(issue.Subject)
+
+		if iLen > idLen {
+			idLen = iLen
+		}
 
 		if sLen > statusLen {
 			statusLen = sLen
 		}
 
-		if iLen > idLen {
-			idLen = iLen
+		if pLen > priorityLen {
+			priorityLen = pLen
+		}
+
+		if proLen > projectLen {
+			projectLen = proLen
 		}
 
 		if subLen > subjectLen {
 			subjectLen = subLen
 		}
 
-		if proLen > projectLen {
-			projectLen = proLen
-		}
 	}
 
-	fmt.Printf("%s  %s  %s  %s\n",
-		"ID "+strings.Repeat(" ", int(math.Abs(float64(idLen-len("ID"))))),
-		"STATUS"+strings.Repeat(" ", int(math.Abs(float64(statusLen-len("STATUS"))))),
-		"PROJECT"+strings.Repeat(" ", int(math.Abs(float64(projectLen-len("PROJECT"))))),
-		"SUBJECT",
+	fmt.Printf("%s   %s  %s  %s  %s\n",
+		HEAD_ID+strings.Repeat(" ", idLen-len(HEAD_ID)),
+		HEAD_STATUS+strings.Repeat(" ", statusLen-len(HEAD_STATUS)),
+		HEAD_PRIORITY+strings.Repeat(" ", priorityLen-len(HEAD_PRIORITY)),
+		HEAD_PROJECT+strings.Repeat(" ", projectLen-len(HEAD_PROJECT)),
+		HEAD_SUBJECT,
 	)
 
 	for _, issue := range issues.Issues {
-		sLeft := statusLen - len(issue.Status.Name)
 		iLeft := idLen - countDigi(issue.ID)
+		sLeft := statusLen - len(issue.Status.Name)
+		pLeft := priorityLen - len(issue.Priority.Name)
 		proLeft := projectLen - len(issue.Project.Name)
 
-		status := issue.Status.Name + strings.Repeat(" ", sLeft) // strconv.Itoa(sLeft)
-		idPad := strings.Repeat(" ", iLeft)
+		id := print.PrintID(issue.ID) + strings.Repeat(" ", iLeft)
+		status := issue.Status.Name + strings.Repeat(" ", sLeft)
+		priority := issue.Priority.Name + strings.Repeat(" ", pLeft)
 		project := issue.Project.Name + strings.Repeat(" ", proLeft)
 
-		fmt.Printf("%s%s  %s  %s %s\n",
-			print.PrintID(issue.ID),
-			idPad,
+		fmt.Printf("%s  %s  %s  %s  %s\n",
+			id,
 			status,
+			priority,
 			project,
 			issue.Subject,
 		)
@@ -190,8 +213,8 @@ func cmdIssueList(r *config.Red_t) *cobra.Command {
 		},
 	})
 
-	cmd.PersistentFlags().Bool(FLAG_ORDER_DESC, false, "desc")
-	cmd.PersistentFlags().StringP(FLAG_SORT, FLAG_SORT_P, "", "Sort field: ID, Status, Project, Subject")
+	cmd.PersistentFlags().Bool(FLAG_ORDER_ASC, false, "Ascend order")
+	cmd.PersistentFlags().StringP(FLAG_SORT, FLAG_SORT_P, "", "Sort field: id, status, priority, project, subject")
 	cmd.PersistentFlags().StringP(FLAG_SEARCH, FLAG_SEARCH_P, "", "Search in subject field")
 	cmd.PersistentFlags().IntP(FLAG_PAGE, FLAG_PAGE_P, 0, "List 25 issues per page (uses limit and offset)")
 	cmd.PersistentFlags().IntP(FLAG_LIMIT, FLAG_LIMIT_P, 25, "Limit number of issues per page")
