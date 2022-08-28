535f9e8b8da26829e93f1e680ffe10b9c1852798
mrjefflarry <mrjefflarry@gmail.com>
2022-08-28 17:27:03 +0200

    test

--- script/apt.sh
+++ script/apt.sh
@@ -19,3 +19,5 @@ gzip -k -f Packages
 apt-ftparchive release . > Release
 gpg --default-key "$EMAIL" -abs -o - Release > Release.gpg
 gpg --default-key "$EMAIL" --clearsign -o - Release > InRelease
+
+cd ../
