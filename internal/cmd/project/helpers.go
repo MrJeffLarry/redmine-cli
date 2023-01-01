88b83db514318bbb5b2500bdd384e769d3161779
mrjefflarry <mrjefflarry@gmail.com>
2023-01-01 20:37:33 +0100

    fix #24

--- internal/cmd/project/helpers.go
+++ internal/cmd/project/helpers.go
@@ -30,3 +30,30 @@ func GetTrackers(r *config.Red_t, projectID int) ([]util.IdName, error) {
 
 	return project.Project.Trackers, nil
 }
+
+func GetVersions(r *config.Red_t, projectID int) ([]util.IdName, error) {
+	var payload versions
+	var versions []util.IdName
+
+	body, status, err := api.ClientGET(r, "/projects/"+strconv.Itoa(projectID)+"/versions.json")
+
+	print.Debug(r, "%d %s", status, string(body))
+
+	if err != nil {
+		return versions, err
+	}
+
+	if err := json.Unmarshal(body, &payload); err != nil {
+		print.Debug(r, err.Error())
+		return versions, errors.New("Could not parse and read response from server")
+	}
+
+	for _, v := range payload.Versions {
+		versions = append(versions, util.IdName{
+			ID:   v.ID,
+			Name: v.Name,
+		})
+	}
+
+	return versions, nil
+}
